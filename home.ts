125b7232b14cd7031e6b3fac0c5b25a7f3d92365
chimereze <chimerezeharrison@gmail.com>
2022-08-10 11:02:33 +0100

    created index.js and home.ts added some variables

---
+++ home.ts
@@ -0,0 +1,7 @@
+const first: string = "John";
+let age: number = 30;
+let lastname: string = "Doe";
+console.warn("Last name variable is in came case", lastname);
+if (typeof(age) !== "number") {
+    console.error("Age is not a number");
+}
